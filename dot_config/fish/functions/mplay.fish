c344bec6281cccbaf57a0d61c45a461d2574511c
brazcom <bassinimattia@gmail.com>
2025-05-31 22:55:00 +0200

    Update dotfiles

--- dot_config/fish/functions/mplay.fish
+++ dot_config/fish/functions/mplay.fish
@@ -1,7 +1,9 @@
 function mplay
     set folder $argv[1]
     if test -z "$folder"
-        set folder .
+        set folder "."
     end
-    find $folder -type f \( -iname "*.mp3" -o -iname "*.flac" -o -iname "*.wav" \) -print0 | sort -z | xargs -0 mpv
+
+    find "$folder" -type f \( -iname "*.mp3" -o -iname "*.flac" -o -iname "*.wav" -o -iname "*.ogg" -o -iname "*.opus" \) -print0 | sort -z | xargs -0 -- mpv
 end
+
