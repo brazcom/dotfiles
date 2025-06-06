acfb52291c7dceb1194005c998c3c18f2c4f775e
brazcom <sobahighroller@gmail.com>
2025-06-06 22:32:01 +0200

    Update dotfiles

---
+++ dot_config/slstatus/config.h
@@ -0,0 +1,19 @@
+/* slstatus configuration */
+/* See LICENSE file for copyright and license details. */
+
+/* interval between updates (in ms) */
+const unsigned int interval = 1000;
+
+/* text to show if no value can be retrieved */
+static const char unknown_str[] = "n/a";
+
+/* maximum output string length */
+#define MAXLEN 2048
+
+/* output formats */
+static const struct arg args[] = {
+	/* function, format, argument */
+  { cpu_perc,     "󰇙󰇙 CPU %s 󰇙󰇙 ", "NULL" },
+  { ram_perc,   "RAM %s 󰇙󰇙 ", "NULL" },
+	{ datetime, "%s", "%T" },
+};
