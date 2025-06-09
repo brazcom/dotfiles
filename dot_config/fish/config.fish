36cc0076cffa5d80a9af981f8cbd559ce9fc7ded
brazcom <sobahighroller@gmail.com>
2025-06-09 15:35:45 +0200

    Update dotfiles

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,11 +1,22 @@
-if test -z "$DISPLAY"; and test "$XDG_VTNR" = "1"
-    exec startx ~/.config/xinitrc
+if status is-login
+    if test -z "$DISPLAY"; and test "$XDG_VTNR" = "1"
+        set -x XAUTHORITY "$XDG_RUNTIME_DIR/Xauthority"
+
+        # Log diagnostico su file
+        echo "Starting X at (date)" >> ~/.config/.xsession.log
+
+        # Avvia startx e salva output su file di log per debug
+        startx ~/.config/.xinitrc >> ~/.config/.xsession.log 2>&1
+
+        # Dopo startx, esci dalla shell
+        exit
+    end
 end
 
 set -x LANG en_US.UTF-8
 set -U fish_greeting
-set -x CARGO_HOME ~/.cache/cargo
-set -x GNUPGHOME ~/.config/gnupg
+set -x CARGO_HOME ~/.cache/.cargo
+set -x GNUPGHOME ~/.config/.gnupg
 
 if status is-interactive
     echo "ciao!!"
